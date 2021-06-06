18a34833c8fe2abfaba767c482cdcaacef0cd91d
TheNootLinja <thenootlinja@gmail.com>
2021-06-06 18:06:46 -0400

    Add changes that weren't supposed to be part of this branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,12 +11,12 @@ export default function Home({ data }) {
     <Layout>
       <div class="main-content">
         <h1>What's up!</h1>
+        <p>
+          This is a place for me to share my dev experience whether I am
+          learning a new framework, found a bug (and hopefully a solution), or I
+          just want to talk about a project I am working on.
+        </p>
       </div>
-      <p>
-        This is a place for me to share my dev experience whether I am learning
-        a new framework, found a bug (and hopefully a solution), or I just want
-        to talk about a project I am working on.
-      </p>
       <Dogs data={data} />
     </Layout>
   )
