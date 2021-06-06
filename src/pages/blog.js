b2a9ddf72a39ddf053d28b374a32277d7dba5c69
TheNootLinja <thenootlinja@gmail.com>
2021-06-06 17:41:13 -0400

    Add a simple hover effect to blog previews and adjust spacing

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -13,7 +13,7 @@ export default function blog({ data }) {
             <h1>Blog Posts</h1>
 
             {posts.map(post => (
-                <article key={post.id}>
+                <article className='blog-preview' key={post.id}>
                 <Link to={"/blog" + post.fields.slug}>
                 <h2>{post.frontmatter.title}</h2>
                 </Link>
