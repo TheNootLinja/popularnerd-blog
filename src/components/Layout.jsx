86d05e05dc3e9f030d231ec08f39cdf68dbe9ba4
TheNootLinja <thenootlinja@gmail.com>
2021-06-09 21:23:55 -0400

    Final styling for simple app

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
     return (
         <div>
             <Header />
-            <div style={{margin: '46px auto 0 auto', minHeight: '1250px', maxWidth: '600px', padding: '0 7px 0 7px'}}>
+            <div style={{margin: '46px auto 0 auto', maxWidth: '600px', padding: '0 7px 0 7px'}}>
                 { children }
             </div>
         </div>
